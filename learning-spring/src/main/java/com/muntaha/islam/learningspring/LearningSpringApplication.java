9fdf7a3b4ca1f19802b9b1665d70483c47929cd1
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2022-01-26 18:58:42 +0600

    Same message as the last commit.

--- learning-spring/src/main/java/com/muntaha/islam/learningspring/LearningSpringApplication.java
+++ learning-spring/src/main/java/com/muntaha/islam/learningspring/LearningSpringApplication.java
@@ -3,6 +3,7 @@ package com.muntaha.islam.learningspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This annotation designates this file as a configuration file as well as starting of all of component scanning.
 @SpringBootApplication
 public class LearningSpringApplication {
 
--- learning-spring/src/test/java/com/muntaha/islam/learningspring/LearningSpringApplicationTests.java
+++ learning-spring/src/test/java/com/muntaha/islam/learningspring/LearningSpringApplicationTests.java
@@ -3,6 +3,7 @@ package com.muntaha.islam.learningspring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Ensures that the application configuration and our application context are able to start up together and work appropriately.
 @SpringBootTest
 class LearningSpringApplicationTests {
 
